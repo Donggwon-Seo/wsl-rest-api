7789edea5c90408a893a85722efdfecdae687b37
Donggwon Seo <ehdrnjsdl920@naver.com>
2022-05-26 17:46:15 +0900

    wsl-REST-API 2차 작업

--- hello-node/app.js
+++ hello-node/app.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const express = require("express");
-const {json} = require("express");
 
 const app = express();
 
@@ -12,8 +11,8 @@ app.use(express.urlencoded({
 app.get('/users', (req, res, next) => {
     let body = '';
     fs.readFile('./db.json', 'utf-8', function (err, data) {
-        //body = data;
-        //res.end(body);
+        body = data;
+        res.end(body);
         res.end("GET Request Success")
         console.log("GET 요청이 수행됨");
     })
@@ -24,17 +23,20 @@ app.post('/login', (req, res) => {
     console.log("Request Success");
     console.log(req.body);  //body 내용을 콘솔에 출력
 
-    fs.readFile('./db.json', 'utf8', (error, jsonFile) => {
+    fs.readFile('./db.json', 'utf8',
+        (error, jsonFile) => {
+        if (error) return console.log(error);
+
         const jsonData = JSON.parse(jsonFile);
         console.log(jsonData);
-       // const user = jsonData.users;
 
+        const users = jsonData.users;
         const {name, password} = req.body;
 
-        for (let idx = 0; idx < user.length; idx++) {
-            const users = user[idx];
-            if (users.id === name) {
-                if (users.password === password) {
+        for (let idx = 0; idx < users.length; idx++) {
+            const user = users[idx];
+            if (user.name === name) {
+                if (user.password === password) {
                     return res.status(200).send("login success");
                 }
             }
